b70a543f641c34f900c50aa897d6b706e2c35f61
codeamac07 <codeamac07@gmail.com>
2016-11-01 17:36:03 -0600

    controladores por separado

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,45 +11,6 @@ get '/' do
   end
 end
 
-post '/user/reg' do
-  @user = User.new(name:params[:name],email:params[:email])
-  @user.password=params[:password]
-  if @user.save
-    session[:user_id]=@user.id
-    return erb :in , layout: false
-  else
-    return erb :error , layout: false
-  end
-end
-
-post '/user/ini' do
-  @user = User.authenticate(params[:email],params[:password])
-  if @user
-    session[:user_id]=@user.id
-    return erb :in , layout: false
-  else
-    return erb :error , layout: false
-  end
-end
-
-post '/user/mod' do
-  p params
-  @user = User.find(session[:user_id])
-  @user.name = params[:name] if params[:name] != ""
-  @user.email = params[:email] if params[:email] != ""
-  @user.password = params[:password] if params[:password] != ""
-  if @user.save
-    @user = User.find(session[:user_id])
-    return erb :in , layout: false
-  else
-    return erb :error , layout: false
-  end
-end
-
-post '/user/out' do
-  session.clear
-  redirect  '/' 
-end
 #===========================
 #== Controlador Principal ==
 #===========================
